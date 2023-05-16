5155cfd1d2e2c1087da73328602484b5a0f698ac
Jan Kennu Paz <jankennupaz@hotmail.com>
2023-05-16 15:50:11 +0800

    added gh pages requirements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 import Nav from "./common/Nav";
